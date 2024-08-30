357c12d6339f95625d6a6dafaef00a4d1bb4ab27
Joyee Cheung <joyeec9h3@gmail.com>
2024-08-30 19:07:35 +0200

    Update generate-report.sh

--- generate-report.sh
+++ generate-report.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -x
 mkdir -p reports
 DATENAME=$(date +"%Y-%m-%d")
 REPORT=reports/$DATENAME.md
@@ -12,6 +12,12 @@ awk '/^### Progress/ {found=1} found' $REPORT >> progress.md
 echo -en "\n" >> progress.md
 
 sed -i '/^### Progress/,$d' $REPORT
+
+git config user.name "Node.js GitHub Bot"
+git config user.email github-bot@iojs.org
+
 git add ./reports
 git commit -m "Add report for $DATENAME"
+git --no-pager log -1 --stat
+
 git push
